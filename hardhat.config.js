0469c23cb60cecfaf60288c2325e283a1166078c
Karthikeya Gummadi <karthik@Karthikeyas-Laptop.local>
2024-10-19 07:07:37 -0700

    m

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,17 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
-const { PRIVATE_KEY, ALCHEMY_API_KEY } = process.env;
+console.log("Loaded Private Key:", process.env.PRIVATE_KEY);
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
   networks: {
     polygon: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${PRIVATE_KEY}`],
-      chainId: 137,
+      url: "https://polygon-rpc.com/",
+      accounts: [process.env.PRIVATE_KEY ? `0x${process.env.PRIVATE_KEY}` : ""],
     },
     polygonMumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${PRIVATE_KEY}`],
-      chainId: 80001,
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY ? `0x${process.env.PRIVATE_KEY}` : ""],
     },
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY,
   },
 };
